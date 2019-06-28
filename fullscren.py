f1a6b59744b710ba14509c9bfa739f42cf89f5c0
SedSec <43684955+SedSec@users.noreply.github.com>
2019-06-28 00:38:22 -0300

    Update fullscren.py

--- fullscren.py
+++ fullscren.py
@@ -20,15 +20,16 @@ dist = raw_input("#: ")
 
 if dist == 1:
 	print("Instalando dependencias...")
-	os.system("apt-get install linux-headers-4.19.0-kali5-all-amd64")
-	os.system("apt-get install linux-image-4.19.0-kali5-amd64")
+	os.system("apt-get install linux-headers-4.19.0-kali5-all -y")
+	os.system("apt-get install linux-image-cloud-amd64 -y")
 	os.system("chmod 777 vbox.run && ./vbox.run")
 
-else dist == 2:
+else:
 	print("Instalando dependencias...")
-	os.system("apt-get install linux-headers-4.19.0-kali5-all")
-	os.system("apt-get install linux-image-cloud-amd64")
+	os.system("apt-get install linux-headers-4.19.0-kali5-all-amd64 -y")
+	os.system("apt-get install linux-image-4.19.0-kali5-amd64 -y")
 	os.system("chmod 777 vbox.run && ./vbox.run")
+	
 os.system("clear")
 print("Reinicie o Computador 2 Vezes")
 time.sleep(7)
